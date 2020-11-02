9951a6fdabc4bf80108b85645ae344676a19469a
Pnina Rosenstark <Pnina@merozsw.co.il>
2020-11-02 23:18:19 +0200

    styles and custom-header

--- src/app/demo-utils/custom-week-view.component.ts
+++ src/app/demo-utils/custom-week-view.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CalendarWeekViewComponent } from 'angular-calendar';
+import { roundToNearest, getMinutesMoved, addDaysWithExclusions } from 'angular-calendar/modules/common/util';
+import {
+  WeekDay,
+  CalendarEvent,
+  WeekViewAllDayEvent,
+  WeekView,
+  ViewPeriod,
+  WeekViewHourColumn,
+  WeekViewTimeEvent,
+  WeekViewHourSegment,
+  WeekViewHour,
+  WeekViewAllDayEventRow,
+} from 'calendar-utils';
+import {
+  DragEndEvent,
+  DropEvent,
+  DragMoveEvent,
+  ValidateDrag,
+} from 'angular-draggable-droppable';
 
 @Component({
   selector: 'custom-week-view',
   templateUrl: 'custom-week-view.component.html',
 })
-export class CustomWeekView extends CalendarWeekViewComponent { }
+export class CustomWeekView extends CalendarWeekViewComponent implements AfterViewInit {
+  isRtl: boolean = true;
+  ngAfterViewInit(): void {
+    // this.isRtl = getComputedStyle(this.element.nativeElement).direction === 'rtl'
+  }
+
+
+}
